93adfdca071e30f790a71caa838c35c1fa6d49dc
caogenwang <709422214@qq.com>
2020-08-28 20:28:22 +0800

    ys

--- File_fread.c
+++ File_fread.c
@@ -4,7 +4,7 @@
 
 int main(int argc,char**argv)
 {
-    FILE fs = fopen("test.txt","rw");
+    FILE *fs = fopen("test.txt","rw");
     if (fs == NULL)
     {
         fclose(fs);
@@ -13,7 +13,7 @@ int main(int argc,char**argv)
     }
     char buf[1024] = {0};
     int n = 0;
-    while ((n=fread(buf,1,5,fs)>0))
+    while ((n=fread(buf,1,5,fs))>0)
     {
         fprintf("%s\n",buf);
     }
