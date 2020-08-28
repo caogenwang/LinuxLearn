e25392b84c9fc299af02f2da2ad4a4fa68413998
caogenwang <709422214@qq.com>
2020-08-28 20:21:51 +0800

    ys

--- File_fread.c
+++ File_fread.c
@@ -4,6 +4,19 @@
 
 int main(int argc,char**argv)
 {
-    
+    FILE fs = fopen("test.txt","rw");
+    if (fs == NULL)
+    {
+        fclose(fs);
+        perror("file open failed");
+        exit(1);
+    }
+    char buf[1024] = {0};
+    int n = 0;
+    while ((n=fread(buf,1,5,fs)>0))
+    {
+        fprintf("%s\n",buf);
+    }
+    fclose(fs);
     return 0;
 }
