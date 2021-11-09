7bf1578f90aa9ea082bcac38e7610f8402ce8b7e
yangsen <yangsen6313@163.com>
2021-11-09 10:34:36 +0800

    ys

--- go/gin/gin_server.go
+++ go/gin/gin_server.go
@@ -2,13 +2,13 @@ package main
 
 import (
     "net/http"
-
+    "fmt"
     "github.com/gin-gonic/gin"
 )
 
 func helloHandler(c *gin.Context) {
     c.JSON(http.StatusOK, gin.H{
-        "message": "Hello www.topgoer.com!",
+        "message": "Hello www.baidu.com!",
     })
 }
 
@@ -16,6 +16,6 @@ func main() {
     r := gin.Default()
     r.GET("/topgoer", helloHandler)
     if err := r.Run(); err != nil {
-        fmt.Println("startup service failed, err:%v\n", err)
+        fmt.Printf("startup service failed, err:%v\n", err)
     }
 }
---
+++ go/gin/go.mod
@@ -0,0 +1,5 @@
+module gin_server
+
+go 1.16
+
+require github.com/gin-gonic/gin v1.7.4
